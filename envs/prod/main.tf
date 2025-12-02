68cce7a381a67e3dd10e9f229dd6fcd9449d6253
sufyan <info@mictrio.com>
2025-12-02 16:43:04 +0500

    prod added

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -1,15 +1,21 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket         = "prod-eks-bucket-747034604262"
+    key            = "eks/prod/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "prod-terraform-locks"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
-  region  = "us-east-2"
+  region  = "us-east-1"
   profile = ""
 
   default_tags {
     tags = {
       Environment = "prod"
-      Project     = "eks-demo"
+      Project     = "eks-max-fun"
       Terraform   = "true"
     }
   }
@@ -19,20 +25,20 @@ module "stack" {
   source = "../.."
 
   env_name          = "prod"
-  project_name      = "eks-demo"
-  aws_region        = "us-east-2"
-  vpc_cidr          = "10.2.0.0/16"
+  project_name      = "eks-max-fun"
+  aws_region        = "us-east-1"
+  vpc_cidr          = "11.0.0.0/16"
   nat_gateway_count = 2
 
   kubernetes_version  = "1.30"
-  node_instance_types = ["m6i.large"]
+  node_instance_types = ["t3.large"]
 
-  node_desired_capacity = 4
-  node_min_size         = 3
-  node_max_size         = 8
+  node_desired_capacity = 2
+  node_min_size         = 2
+  node_max_size         = 4
 
   node_capacity_type          = "ON_DEMAND"
-  cluster_public_access_cidrs = ["198.51.100.0/24"] # Prod: strictly limited
+  cluster_public_access_cidrs = ["0.0.0.0/0"] # Prod: strictly limited
 
   enable_irsa                  = true
   enable_ssm_on_nodes          = true
